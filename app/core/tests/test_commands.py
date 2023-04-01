c1ef8db05c877b139cd0f2e9f6ef78989f81ee64
yahyaobeid <yobeid@sfsu.edu>
2023-03-31 18:01:06 -0700

    Fixed linting errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,23 +12,23 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-	"""Test commands."""
+    """Test commands."""
 
-	def test_wait_for_db_ready(self, patched_check):
-		"""Test waiting for database if database is ready."""
-		patched_check.return_value = True
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database is ready."""
+        patched_check.return_value = True
 
-		call_command('wait_for_db')
+        call_command('wait_for_db')
 
-		patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
-	@patch('time.sleep')
-	def test_wait_for_db_delay(self, patched_sleep, patched_check):
-		"""Test waiting for database when getting Operational Error"""
-		patched_check.side_effect = [Psycopg2OpError] * 2 + \
-		  [OperationalError] * 3 + [True]
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting Operational Error"""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+            [OperationalError] * 3 + [True]
 
-		call_command('wait_for_db')
+        call_command('wait_for_db')
 
-		self.assertEqual(patched_check.call_count, 6)
-		patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
