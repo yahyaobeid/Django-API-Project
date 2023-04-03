d62ad251d2ef550a4e47afd4283415c3c33ed98e
yahyaobeid <yobeid@sfsu.edu>
2023-04-03 14:00:01 -0700

    fixed linting issue

--- app/core/admin.py
+++ app/core/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
         (
             _('Permissions'),
             {
-                'fields':(
+                'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
