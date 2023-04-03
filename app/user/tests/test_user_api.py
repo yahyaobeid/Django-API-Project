01855a2267ac8dd42f96bce898e482bae1b42222
yahyaobeid <yobeid@sfsu.edu>
2023-04-03 14:47:02 -0700

    fixed test errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -53,13 +53,13 @@ class PublicUserAPITests(TestCase):
         """Test and error is returned if password is less than 5 chars."""
         payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
+            'password': 'pw',
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = user.get_user_model().objects.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
